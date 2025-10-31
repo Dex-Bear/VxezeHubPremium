651549709c107507d77ed00badf90de7e77383c8
Dex Skibidi <anhthinhcz@gmail.com>
2025-10-31 14:10:33 +0700

    Add Blox Fruits game support in GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+--Blox Fruits
+getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BloxFruit.lua"))()
+end
