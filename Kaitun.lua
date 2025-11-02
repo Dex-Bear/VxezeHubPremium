05415ee7f929561e741bd1a7cd05f1f90421d7a1
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-02 14:41:51 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -2,4 +2,7 @@ local gameId = game.PlaceId
 if _G.KaitunMode == "Get Race" and (gameId == 2753915549 or gameId == 4442272183 or gameId == 100117331123089) then
   -- BloxFruit
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/GetRace.lua"))()
+elseif _G.KaitunMode == "Auto Fruit" and (gameId == 2753915549 or gameId == 4442272183 or gameId == 100117331123089) then
+  -- BloxFruit
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/AutoFruit.lua"))()
 end
