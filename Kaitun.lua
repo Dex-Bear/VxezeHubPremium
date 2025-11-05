3dac0e5436f3f2e3344ed86d608ec7729464a5b8
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-05 21:26:19 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -1,8 +1,11 @@
 local gameId = game.PlaceId
-if _G.KaitunMode == "Get Race" and (gameId == 2753915549 or gameId == 4442272183 or gameId == 100117331123089) then
+if _G.KaitunMode == "Get Race" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
   -- BloxFruit
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/GetRace.lua"))()
-elseif _G.KaitunMode == "Auto Fruit" and (gameId == 2753915549 or gameId == 4442272183 or gameId == 100117331123089) then
+elseif _G.KaitunMode == "Auto Fruit" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
   -- BloxFruit
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/AutoFruit.lua"))()
+elseif _G.KaitunMode == "TSB" and (gameId == 10449761463) then
+  -- Tsb
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/TSB.lua"))()
 end
