5f8f0003b2f17212715362d89d91eb7ee6fd3d47
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-08 23:04:39 +0700

    Add KaitunBossHop for Auto Fruit mode
    Added KaitunBossHop loading for Auto Fruit mode.

--- Kaitun.lua
+++ Kaitun.lua
@@ -5,6 +5,9 @@ if _G.KaitunMode == "Get Race" and (gameId == 85211729168715 or gameId == 790917
 elseif _G.KaitunMode == "Auto Fruit" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
   -- BloxFruit
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/AutoFruit.lua"))()
+elseif _G.KaitunMode == "Auto Fruit" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
+  -- BloxFruit
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/KaitunBossHop.lua"))()
 elseif _G.KaitunMode == "TSB" and (gameId == 10449761463) then
   -- Tsb
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/TSB.lua"))()
