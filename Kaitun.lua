ffa03e5f44d5929c2cd9eb4940977321049e46c2
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-09 21:30:18 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -7,11 +7,14 @@ elseif _G.KaitunMode == "Auto Fruit" and (gameId == 85211729168715 or gameId == 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/AutoFruit.lua"))()
 elseif _G.KaitunMode == "Boss Hop" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
   -- BloxFruit
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/KaitunBossHop.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BossHop.lua"))()
 elseif _G.KaitunMode == "TSB" and (gameId == 10449761463) then
   -- Tsb
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/TSB.lua"))()
 elseif _G.KaitunMode == "Fish It!" and (gameId == 121864768012064) then
   -- Fish It!
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FishIt!.lua"))()
+elseif _G.KaitunMode == "Mm2" and (gameId == 80469437126309) then
+  -- Fish It!
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/Mm2.lua"))()
 end
