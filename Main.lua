a85c38b4d3553fcfebc4e0a500206a5c57b12b01
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-29 16:08:09 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,6 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/GameList.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
+local placeId = game.PlaceId
+if placeId == 85211729168715 or placeId == 79091703265657 or placeId == 100117331123089 then
+    -- Blox Fruits
+    _G.Team = "Marines"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BloxFruit.lua"))()
 end
