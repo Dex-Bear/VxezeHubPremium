919d6c67e8b5373883621cb8f1aa522193d2ab8f
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-29 16:04:53 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 --Blox Fruits
-getgenv().Team = "Marines"
+_G.Team = "Marines"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BloxFruit.lua"))()
 end
