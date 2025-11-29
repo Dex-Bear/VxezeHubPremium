2504069624d56846218a452ea73177d43172aa46
Dex Skibidi <anhthinhcz@gmail.com>
2025-11-29 16:07:32 +0700

    Delete GameList.lua

--- GameList.lua
+++
@@ -1,6 +0,0 @@
-local placeId = game.PlaceId
-if placeId == 85211729168715 or placeId == 79091703265657 or placeId == 100117331123089 then
---Blox Fruits
-_G.Team = "Marines"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BloxFruit.lua"))()
-end
