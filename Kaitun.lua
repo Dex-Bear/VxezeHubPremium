86287b6d380f3ef30b7a49bd9466a22f3de143d5
Dex Skibidi <anhthinhcz@gmail.com>
2025-12-13 13:45:08 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -1,5 +1,5 @@
 local GameConfig = {
-    ["BloxFruit"] = {
+    ["Blox Fruit"] = {
         ids = {85211729168715, 79091703265657, 100117331123089},
         scripts = {
             ["Race"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/GetRace.lua",
@@ -7,7 +7,7 @@ local GameConfig = {
             ["Boss Hop"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BossHop.lua"
         }
     },
-    ["TSB"] = {
+    ["The Strongest Battlegrouds"] = {  
         ids = {10449761463},
         scripts = {
             ["Tsb"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/TSB.lua"
@@ -19,10 +19,62 @@ local GameConfig = {
             ["Fish It!"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FishIt!.lua"
         }
     },
-    ["Mm2"] = {
+    ["Murder Mystery 2"] = {
         ids = {80469437126309},
         scripts = {
             ["Mm2"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/Mm2.lua"
+        }
+    },
+    ["Steal A Brainrot"] = {
+        ids = {96342491571673},
+        scripts = {
+            ["Steal A Brainrot"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/SAB.lua"
+        }
+    }
+}
+local gameId = game.PlaceId
+for gameName, config in pairs(GameConfig) do
+    if table.find(config.ids, gameId) then
+        if _G.Kaitun and config.scripts[_G.Kaitun] then
+            local scriptUrl = config.scripts[_G.Kaitun]
+            pcall(function()
+                loadstring(game:HttpGet(scriptUrl))()
+            end)
+        end
+        break
+    end
+end
+local GameConfig = {
+    ["Blox Fruit"] = {
+        ids = {85211729168715, 79091703265657, 100117331123089},
+        scripts = {
+            ["Race"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/GetRace.lua",
+            ["Collect"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/AutoFruit.lua",
+            ["Boss Hop"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BossHop.lua"
+        }
+    },
+    ["The Strongest Battlegrouds"] = {  
+        ids = {10449761463},
+        scripts = {
+            ["Tsb"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/TSB.lua"
+        }
+    },
+    ["Fish It!"] = {
+        ids = {121864768012064},
+        scripts = {
+            ["Fish It!"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FishIt!.lua"
+        }
+    },
+    ["Murder Mystery 2"] = {
+        ids = {80469437126309},
+        scripts = {
+            ["Mm2"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/Mm2.lua"
+        }
+    },
+    ["Steal A Brainrot"] = {
+        ids = {96342491571673},
+        scripts = {
+            ["Steal A Brainrot"] = "https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/SAB.lua"
         }
     }
 }
