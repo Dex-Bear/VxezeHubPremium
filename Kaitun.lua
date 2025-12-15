d5d04a8fa25181d1764a13810c39b69d3888b21a
Dex Skibidi <anhthinhcz@gmail.com>
2025-12-15 22:26:50 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -1,15 +1,12 @@
 local gameId = game.PlaceId
-
-if _G.Kaitun == "Race" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/GetRace.lua"))()
-elseif _G.Kaitun == "Find Fruit" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
+if _G.Kaitun == "Find Fruit" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FindFruit.lua"))()
-elseif _G.Kaitun == "Boss Hop" and (gameId == 85211729168715 or gameId == 79091703265657 or gameId == 100117331123089) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BossHop.lua"))()
 elseif _G.Kaitun == "Fish It!" and gameId == 121864768012064 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FishIt!.lua"))()
 elseif _G.Kaitun == "Mm2" and gameId == 80469437126309 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/Mm2.lua"))()
 elseif _G.Kaitun == "Steal A Brainrot" and gameId == 96342491571673 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/SAB.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Script not supported for this game")
 end
