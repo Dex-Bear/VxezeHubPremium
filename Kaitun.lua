570e0e9aa1b88d34f076940355a242a55c305d1e
Dex Skibidi <anhthinhcz@gmail.com>
2025-12-16 09:12:23 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -3,7 +3,7 @@ if _G.Kaitun == "Find Fruit" and (gameId == 85211729168715 or gameId == 79091703
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FindFruit.lua"))()
 elseif _G.Kaitun == "Fish It!" and gameId == 121864768012064 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/FishIt!.lua"))()
-elseif _G.Kaitun == "Mm2" and gameId == 80469437126309 then
+elseif _G.Kaitun == "Mm2" and gameId == 80469437126309 or gameId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/Mm2.lua"))()
 elseif _G.Kaitun == "Steal A Brainrot" and gameId == 96342491571673 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/SAB.lua"))()
