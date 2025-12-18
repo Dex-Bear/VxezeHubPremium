ca99bd4cd779cd59ae8ce70d39ec0b89cefbcf21
Dex Skibidi <anhthinhcz@gmail.com>
2025-12-18 10:48:08 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
-local placeId = game.PlaceId
-if placeId == 85211729168715 or placeId == 79091703265657 or placeId == 100117331123089 then
-    -- Blox Fruits
+local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
+if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 or UniverseID == 994732206 then
+        -- Blox Fruits
     _G.Team = "Marines"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubPremium/refs/heads/main/BloxFruit.lua"))()
 end
