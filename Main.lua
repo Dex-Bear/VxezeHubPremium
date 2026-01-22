29b3f92f5125fac4c08536b0bd9313f7aac52894
Dex Skibidi <anhthinhcz@gmail.com>
2026-01-22 22:23:36 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,15 +1,3 @@
--- Check Executor Support
-local plr = game.Players.LocalPlayer
-
-if (hookfunction and hookmetamethod and getconnections and cloneref and firetouchinterest and fireproximityprompt and collectgarbage and version and gethwid) then
-    print("Supported Executor!")
-else
-    plr:Kick("Missing hookfunction/hookmetamethod...")
-    local r = rawget(getrawmetatable(plr), "Kick") or plr.Kick
-    r(plr, "")
-    game:Shutdown()
-end
-
 -- Check Game ID
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 
